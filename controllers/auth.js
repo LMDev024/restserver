94e084f2dd5e9fe3f6f0a4228a40e57fe1dbfd29
Luis Malagon <luismalagondev@gmail.com>
2022-02-05 17:06:25 -0500

    Google sigIn añadido

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const { response } = require("express");
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 const Usuario = require('../models/usuario');
 
 const login = async(req, res=response)=>{
@@ -50,6 +51,48 @@ const login = async(req, res=response)=>{
 
 }
 
+const googleSignIn = async(req, res= response) =>{
+    const {id_token}= req.body;
+    try {
+        const {correo, nombre, img} =  await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({correo});
+
+        if( !usuario ){
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                //COMO ASIGNAR UN ROLE?
+                rol: 'USER_ROLE',
+                google:true
+            }
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg:'Hable con el administrador, usuario bloqueado'
+            });
+        }
+        //generar el JWT (Json Web Token)
+
+        const token = await generarJWT( usuario.id );
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok : false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
